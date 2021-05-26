a504449f8a1e45b55aac558f13d1f06b19870e62
Michelle Verge <michellelverge@gmail.com>
2021-05-26 14:35:42 +0000

    returns separately only stores carrying mens apparel and only stores carrying womens apparel(revenue under 1 million)

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,24 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where("mens_apparel = true")
+
+for i in @mens_stores
+  puts i.name
+  puts i.annual_revenue
+end
+
+
+@womens_stores = Store.where("womens_apparel = true")
+
+low_revenue = @womens_stores.where("annual_revenue < 1000000")
+
+for i in low_revenue
+  puts i.name
+  puts i.annual_revenue
+end
