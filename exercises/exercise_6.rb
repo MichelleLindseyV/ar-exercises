babf40d5e3729e492cf5e98f0324f45f38bd67ca
Michelle Verge <michellelverge@gmail.com>
2021-05-26 14:59:35 +0000

    tells active record how store and employee tables are related and adds employees to stores 1 & 2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Kiram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Grace", last_name: "Kelley", hourly_rate: 50)
+@store1.employees.create(first_name: "Niko", last_name: "Manos", hourly_rate: 30)
+
+@store1.employees.create(first_name: "Seth", last_name: "Rogen", hourly_rate: 100)
+@store1.employees.create(first_name: "John", last_name: "Oliver", hourly_rate: 40)
+@store1.employees.create(first_name: "Sheldon", last_name: "Cooper", hourly_rate: 80)
